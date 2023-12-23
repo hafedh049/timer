c00ea230ef67adcc23dbe28f75607d11be138be9
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-23 21:33:11 +0100

    Update

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color dark = Color.fromARGB(255, 40, 42, 44);
 const Color purple = Color.fromARGB(255, 244, 180, 220);
 const Color white = Color.fromARGB(255, 215, 218, 244);
+const Color transparent = Colors.transparent;
